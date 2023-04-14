24d38b01e1c480f439580188cc7c6a8f301b4635
Timothy Makkison <timmakkison@gmail.com>
2023-04-14 10:09:57 +0100

    fix: refactor

--- src/Riok.Mapperly/Descriptors/Enumerables/EnsureCapacityBuilder.cs
+++ src/Riok.Mapperly/Descriptors/Enumerables/EnsureCapacityBuilder.cs
@@ -52,41 +52,43 @@ public class EnsureCapacityBuilder
             && x.ReturnType.SpecialType == SpecialType.System_Int32
             && !x.IsStatic);
 
-        // if EnsureCapacity is not available then return false.
+        // if EnsureCapacity is not available then return false
         if (capacityMethod is null)
         {
             return null;
         }
 
-        // if target does not have a count then return false.
+        // if target does not have a count then return false
         if (!TryGetNonEnumeratedCount(targetType, types, out var targetSizeProperty))
         {
             return null;
         }
 
-        // if source does not have a count/length then get information to get the non enumerated count at runtime.
-        if (!TryGetNonEnumeratedCount(sourceType, types, out var sourceSizeProperty))
+        // if target and source count are known then create a simple EnsureCapacity statement
+        if (TryGetNonEnumeratedCount(sourceType, types, out var sourceSizeProperty))
         {
-            sourceType.ImplementsGeneric(types.IEnumerableT, out var iEnumerable);
-
-            var nonEnumeratedCountMethod = types.Enumerable.GetMembers(TryGetNonEnumeratedCountMethodName)
-            .OfType<IMethodSymbol>()
-            .FirstOrDefault(x => x.ReturnType.SpecialType == SpecialType.System_Boolean && x.IsStatic && x.Parameters.Length == 2 && x.IsGenericMethod);
-
-            if (nonEnumeratedCountMethod is not null)
-            {
-                var typedNonEnumeratedCount = nonEnumeratedCountMethod.Construct(iEnumerable!.TypeArguments.ToArray());
-                return new EnsureCapacityBuilder(targetSizeProperty, typedNonEnumeratedCount);
-            }
-
-            var collectionType = ParseTypeName(types.ICollectionT.Construct(iEnumerable!.TypeArguments.ToArray()).ToDisplayString());
-
-            var readonlyCollectionType = ParseTypeName(types.IReadOnlyCollectionT.Construct(iEnumerable.TypeArguments.ToArray()).ToDisplayString());
-
-            return new EnsureCapacityBuilder(targetSizeProperty, collectionType, readonlyCollectionType);
+            return new EnsureCapacityBuilder(targetSizeProperty, sourceSizeProperty);
         }
 
-        return new EnsureCapacityBuilder(targetSizeProperty, sourceSizeProperty);
+        sourceType.ImplementsGeneric(types.IEnumerableT, out var iEnumerable);
+
+        var nonEnumeratedCountMethod = types.Enumerable.GetMembers(TryGetNonEnumeratedCountMethodName)
+        .OfType<IMethodSymbol>()
+        .FirstOrDefault(x => x.ReturnType.SpecialType == SpecialType.System_Boolean && x.IsStatic && x.Parameters.Length == 2 && x.IsGenericMethod);
+
+        // if source does not have a count and GetNonEnumeratedCount is available then EnusreCapacity if count is available
+        if (nonEnumeratedCountMethod is not null)
+        {
+            var typedNonEnumeratedCount = nonEnumeratedCountMethod.Construct(iEnumerable!.TypeArguments.ToArray());
+            return new EnsureCapacityBuilder(targetSizeProperty, typedNonEnumeratedCount);
+        }
+
+        // if source does not have a count and GetNonEnumeratedCount is not available in the current version then try to get lengths at runtime
+        var collectionType = ParseTypeName(types.ICollectionT.Construct(iEnumerable!.TypeArguments.ToArray()).ToDisplayString());
+
+        var readonlyCollectionType = ParseTypeName(types.IReadOnlyCollectionT.Construct(iEnumerable.TypeArguments.ToArray()).ToDisplayString());
+
+        return new EnsureCapacityBuilder(targetSizeProperty, collectionType, readonlyCollectionType);
     }
 
     private static bool TryGetNonEnumeratedCount(ITypeSymbol value, WellKnownTypes types, [NotNullWhen(true)] out string? expression)
@@ -121,9 +123,10 @@ public class EnsureCapacityBuilder
         if (_getNonEnumeratedMethod is not null)
         {
             var countIdentifier = Identifier(ctx.NameBuilder.New("sourceCount"));
-            var lengthIdent = IdentifierName(countIdentifier);
+            var countIdentifierName = IdentifierName(countIdentifier);
 
-            var args = Argument(
+            var enumerableArgument = Argument(ctx.Source);
+            var outVarArgument = Argument(
                                    DeclarationExpression(
                                        IdentifierName(Token(SyntaxKind.VarKeyword)),
                                        SingleVariableDesignation(
@@ -132,10 +135,8 @@ public class EnsureCapacityBuilder
                                    Token(SyntaxKind.OutKeyword));
 
 
-            var aru = Argument(ctx.Source);
-
-            var s = StaticInvocation(_getNonEnumeratedMethod, aru, args);
-            var state = IfStatement(s, Block(EnsureCapacityStatement(target, lengthIdent, targetCount)));
+            var getNonEnumeratedInvocation = StaticInvocation(_getNonEnumeratedMethod, enumerableArgument, outVarArgument);
+            var state = IfStatement(getNonEnumeratedInvocation, Block(EnsureCapacityStatement(target, countIdentifierName, targetCount)));
             return state;
         }
 
